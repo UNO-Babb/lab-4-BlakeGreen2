8ea30fd4bae07c74e71169d7adc23d697a77e07f
BlakeGreen2 <gbjg07@gmail.com>
2025-09-17 13:35:56 -0500

    done with concentric and polygon

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,7 +1,7 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Blake Green 
+#Date: 9/17/2025
+#Assignment: turtle movements
 
 import turtle #needed generally but not in CodeHS
 hideturtle() #hides the default turtle in CodeHS
@@ -12,16 +12,56 @@ def drawSquare(myTurtle, size):
         myTurtle.right(90)
 
 
+def drawPolygon(myTurtle, sides, size=100):
+    angle = 360 / sides
+    for i in range(sides):
+        myTurtle.forward(size)
+        myTurtle.right(angle)
+
+
+def fillCorner(myTurtle, corner, size=100):
+    drawSquare(myTurtle, size)
+    half = size / 2
+    quarter = size / 2
+    myTurtle.penup()
+    if corner == 1:
+        myTurtle.goto(0,0)
+    elif corner == 2:
+        myTurtle.goto(0,half)
+    elif corner == 3:
+        myTurtle.goto(0, 0)
+    elif corner == 4:
+        myTurtle.goto(-half, 0)
+    
+    myTurtle.pendown()
+    myTurtle.begin_fill()
+    drawSquare(myTurtle, size/2)
+    myTurtle.end_fill()
+    myTurtle.penup()
+    
+
+def squaresInSquares(myTurtle, num, size=50, step=20):
+    for i in range(num):
+        drawSquare(myTurtle, size + (i * 20))
+        myTurtle.penup()
+        myTurtle.backward(step / 2)
+        myTurtle.left(90)
+        myTurtle.forward(step / 2)
+        myTurtle.right(90)
+        myTurtle.pendown()
+
 def main():
     myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
+    myTurtle.speed(5)
+    
+    #drawPolygon(myTurtle, 5) #draws a pentagon
+    #drawPolygon(myTurtle, 8) #draws an octogon
 
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
+    fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
+    #fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
 
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
+    #squaresInSquares(myTurtle, 5) #draws 5 concentric squares
+    #squaresInSquares(myTurtle, 3) #draws 3 concentric squares
 
 
 main()
